c03061078f0f3ff9edef320f006593a402c543a9
Konstantin <woodprop@yandex.ru>
2019-08-14 18:40:37 +0300

    menu item -blog- deleted

--- resources/views/includes/menu_items.blade.php
+++ resources/views/includes/menu_items.blade.php
@@ -2,6 +2,5 @@
     <li><a href="{{ route('portfolio') }}"><span class="rL">Портфолио</span></a></li>
     <li><a href="{{ route('portfolio') }}"><span class="rL">Услуги</span></a></li>
     <li><a href="{{ route('about') }}"><span class="rL">О нас</span></a></li>
-    <li><a href="{{ route('portfolio') }}"><span class="rL">Блог</span></a></li>
     <li><a href="{{ route('order') }}"><span class="rL">Заказать проект</span></a></li>
 </ul>
